303e38a7cbedec2e9411b88889e328829728eb95
rob <robiniscoding@gmail.com>
2020-12-20 18:01:05 +0100

    Resolve some errors

--- tp_08/src/main/java/bigdata/HadoopVersion.java
+++ tp_08/src/main/java/bigdata/HadoopVersion.java
@@ -8,7 +8,12 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
-import org.apache.hadoop.hbase.client.*;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
@@ -16,7 +21,9 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
@@ -51,20 +58,14 @@ public class HadoopVersion {
 
     
     public static class SimpleReducer extends Reducer<Text, NullWritable, NullWritable, NullWritable> {
-        HTable table;
+        Table table;
         int row;
         
         public void setup(Context context) throws IOException {
-            table = new HTable(
-                context.getConfiguration(),
-                TABLE_NAME
-            );
+            Connection connection = ConnectionFactory.createConnection(context.getConfiguration());
 
+            table = connection.getTable(TableName.valueOf(TABLE_NAME));
             row = 0;
-        }
-
-        public void cleanup(Context context) throws IOException {
-            table.close();
         }
 
         private void insertOne(String line, String row) {
@@ -143,7 +144,10 @@ public class HadoopVersion {
 		
 		job.setInputFormatClass(TextInputFormat.class);
 		FileInputFormat.addInputPath(job, new Path("/users/robin/worldcitiespop.txt"));
-		
+        
+        job.setOutputFormatClass(TextOutputFormat.class);
+		FileOutputFormat.setOutputPath(job, new Path("/users/robin/test-tp-08"));
+
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
 }
